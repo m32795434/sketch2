c0a64e426f5a502fc3d77d9b89963dfeb8fc0708
Manuel Augusto Bravard <manubravard@gmail.com>
2023-06-25 10:03:48 -0300

    //width: 790px;height: 300px;ok

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -65,10 +65,10 @@ function handleEvent(event, localX, localY) {
     y = event.touches[0].clientY - canvas.offsetTop;
   }
   const visualWidth = window.visualViewport.width;
-  if (visualWidth >= 1024 && visualWidth <= 1180) {
+  if (visualWidth >= 1024 && visualWidth <= 1280) {
     draw(x * 2.7368, y * 2.6666);//width: 950px; height: 450px;ok
-  } else if (visualWidth >= 844 && visualWidth < 1023) {
-    draw(x * 3.79146, y * 4.4444);
+  } else if (visualWidth >= 844 && visualWidth < 1023) {//width: 790px;height: 300px;
+    draw(x * 3.2911, y * 4);
   } else {
     // desktop
     draw(x * 2, y * 2);//width: 1300px;height: 600px;ok
